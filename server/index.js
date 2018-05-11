63e52be83e2beddee55b29b9dd43baed4f7d5930
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-05-11 12:39:00 +0400

    added morgan support, to restart server on each file change

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const morgan = require('morgan');  // logging framework
+
+// nodemon is for automatically restarting server on file change
+
 
 const app = express();
 // App setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server setup
 
