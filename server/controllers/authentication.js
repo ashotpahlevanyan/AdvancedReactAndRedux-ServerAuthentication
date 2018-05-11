47991d2426494b816ede89628cf69c568e06858e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-05-11 16:24:42 +0400

    added existing user search part

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,18 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-	res.send({ success: true });
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// See if the user with a given email exists
+	User.findOne({ email: email}, function(err, existingUser) {
+
+	});
+
+	// If a user with a given email does exist, return error
+
+	// If a user with a given email doesn't exist, create and save user record
+
+	// Respond to request indicating the user was created
+
 };
