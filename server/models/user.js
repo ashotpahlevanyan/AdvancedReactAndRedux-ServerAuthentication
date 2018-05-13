20149264eae9ad56e5d775e24ad2931248c9cbc4
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-05-13 19:22:01 +0400

    bcrypt is used to hash password with salt

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+
 
 // Define our model
 const userSchema  = new Schema({
@@ -7,6 +10,30 @@ const userSchema  = new Schema({
 	password: String
 });
 
+// On save hook encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function (next) {
+	// Get access to the user model
+	const user = this;
+
+	// generate a salt then run callback
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) {
+			return next(err);
+		}
+
+		// hash/encrypt our password using the salt
+		bcrypt.hash(user.password, salt, null, function (err, hash) {
+			if(err) {
+				return next(err);
+			}
+
+			// overwrite plain text password with encrypted password
+			user.password = hash;
+			next();
+		})
+	})
+});
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
